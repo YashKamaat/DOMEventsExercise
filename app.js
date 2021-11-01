5e27868dbf92294a36f96518a0b9bd7f595afee7
Yash Kamaat <yashtkamaat@gmail.com>
2021-11-01 10:21:57 -0400

    Edited with temp. literals.

--- app.js
+++ app.js
@@ -2,11 +2,11 @@
 
 // 1a. Select the 1st button element and set it to a const variable called button1
 //===========================================
-const button1 = document.getElementById('one');
+const button1 = document.getElementById('#one');
 //===========================================
 //1b. Using the ONCLICK event property, have an alert that says "Cool! You can hear/understand a hummingbird and ants" pop up when you click on the button
 //===========================================
-button1.onclick = () => alert("Cool!");
+button1.onclick = () => alert("Cool!You can hear/understand a hummingbird and ants");
 //===========================================
 //2a. Select the h3 and set it to a const variable called h3
 //===========================================
@@ -14,32 +14,45 @@ const h3 = document.querySelector(`h3`);
 //===========================================
 //2b. Using addEventListener, have an alert that says "OH NO! The squirrels are on to you. Now you have to leave this reality and move to a new one" pop up when you hover over the h3
 //===========================================
-h3.addEventListener('hover', () => alert("OH NO! The squirrels are on to you. Now you have to leave this reality and move to a new one"));
+h3.addEventListener(`mouseenter`, () => { 
+    alert("OH NO! The squirrels are on to you. Now you have to leave this reality and move to a new one");
+});
 //===========================================
 //3a. Select the form element/tag and set it to a const variable called form
 //===========================================
-const form = document.querySelector('form');
+const form = document.querySelector(`form`);
 //===========================================
 //3b. Using addEventListener, display the value in the text input with an alert when the submit input in the form is pressed.
 //===========================================
 form.addEventListener(`submit`, () => {
-const formq = form.elements.quote.value;
-alert(formq);
+alert(`${form.elements.quote.value}`);
 });
+
+// OR
+// form.addEventListener(`submit`, q => {
+//   q.preventDefault();
+//   alert(`${form.elements.quote.value}`);
+//   form.elements.quote.value = ``;
+// });
 //===========================================
 
 
 //BONUS
 //4a. Select the Dark Mode button and set it to a const variable called darkMode
 //===========================================
-const darkMode = document.getElementById('dm');
+
+// const darkMode = document.getElementById(`dm`);
+// OR
+const darkMode = document.querySelector(`#dm`);
 //===========================================
 //4b. Using addEventListener, TOGGLE the "dark-mode" CSS class On/Off for EVERY element when the Dark Mode Button is clicked (HINT: You will need to use the asterisk "*" selector as well as a loop)
 //===========================================
 const all = document.querySelectorAll('*');
-darkMode.addEventListener('click', () => {
+darkMode.addEventListener(`click`, () => {
    for (element of all){
-       element.classList.toggle('dark-mode')
+       element.classList.toggle(`dark-mode`);
+   }
+});
 //===========================================
 //5a. Select the "MOVE TO ANOTHER REALITY" button and set it to a const variable called reality
 //===========================================
